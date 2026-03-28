33acd517c32ef55334434dd7e6003ec6f637bed1
omkar1108-hash <omkarkasar1108@gmail.com>
2026-03-28 20:55:39 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function saveNote(forceUpdate = false) {
         return;
     }
 
-    fetch("http://127.0.0.1:5000/add-note", {
+    fetch("/add-note", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
