1f45cbb303f6e7d8ddac55394a13357a319fee8d
omkar1108-hash <omkarkasar1108@gmail.com>
2026-03-28 20:16:20 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ function login() {
 
     error.textContent = "";
 
-    fetch("http://127.0.0.1:5000/api/login", {
+    fetch("/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password })
