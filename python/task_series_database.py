35f8b78292209968718e416a52521eb20b2fa8f8
omkar1108-hash <omkarkasar1108@gmail.com>
2026-03-28 18:09:51 +0530

    Update task_series_database.py

--- python/task_series_database.py
+++ python/task_series_database.py
@@ -1,4 +1,8 @@
 import sqlite3
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "..", "database", "task_series.db")
 
 def get_task_series_connection():
-    return sqlite3.connect("../database/task_series.db")
+    return sqlite3.connect(DB_PATH)
