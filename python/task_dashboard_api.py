8cd93f93d0ef431149b2f5f56a5d95b77c03d808
omkar1108-hash <omkarkasar1108@gmail.com>
2026-03-28 18:11:21 +0530

    Update task_dashboard_api.py

--- python/task_dashboard_api.py
+++ python/task_dashboard_api.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from task_database import get_task_connection
+from .task_database import get_task_connection
 from task_series_database import get_task_series_connection
 
 task_dashboard_bp = Blueprint("task_dashboard_bp", __name__)
