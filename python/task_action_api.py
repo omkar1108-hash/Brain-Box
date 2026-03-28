91abf92e265ffbe18753323b38fa10e3e684987c
omkar1108-hash <omkarkasar1108@gmail.com>
2026-03-28 18:14:04 +0530

    Update task_action_api.py

--- python/task_action_api.py
+++ python/task_action_api.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from datetime import datetime
-from task_database import get_task_connection
-from task_series_database import get_task_series_connection
+from .task_database import get_task_connection
+from .task_series_database import get_task_series_connection
 
 task_action_bp = Blueprint("task_action_bp", __name__)
 print("🔥 task_action_api.py LOADED")
